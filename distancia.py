66550a4a0fef5010e07f12cf72e497072471309f
unknown <gabisa787@gmail.com>
2024-05-06 07:51:16 -0300

    v0.2

--- distancia.py
+++ distancia.py
@@ -1,9 +1,9 @@
 # Recebe a distância e a velocidade do usuário
 distancia = float(input("Digite a distância da viagem em quilômetros: "))
 velocidade = float(input("Digite a velocidade média da viagem em km/h: "))
-
+nomemotorista = input("Digite o nome do motorista: ")
 # Calcula o tempo de viagem
 tempo = distancia / velocidade
 
 # Exibe o tempo de viagem na tela
-print(f"O tempo estimado de viagem é de {tempo:.2f} horas.")
+print(f"{nomemotorista} O tempo estimado de viagem é de {tempo:.2f} horas.")
